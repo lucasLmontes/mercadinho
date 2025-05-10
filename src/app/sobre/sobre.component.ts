4fb087d371995bee5c4a6159b91c3ee3f16cc23c
Lucas Leite <lucasleite2mil4@gmail.com>
2025-05-09 23:58:14 -0300

    adicionando tela 'sobre'

---
+++ src/app/sobre/sobre.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  standalone: false,
+  templateUrl: './sobre.component.html',
+  styleUrl: './sobre.component.scss'
+})
+export class SobreComponent {
+
+}
